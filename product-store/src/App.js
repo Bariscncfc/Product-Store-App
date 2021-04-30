e86efe991619425db41a65e8294735009274bd6a
Barış Can Çifçi <17030220013@student.beykent.edu.tr>
2021-04-30 13:43:38 +0200

    setup app

---
+++ product-store/src/App.js
@@ -0,0 +1,26 @@
+import { Col, Container, Row } from "reactstrap";
+import CategoryList from "./CategoryList";
+import Navi from "./Navi";
+import ProductList from "./ProductList";
+
+function App() {
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Navi />
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList />
+          </Col>
+          <Col xs="9">
+            <ProductList />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
